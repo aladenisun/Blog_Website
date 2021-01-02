f6842d63d527045327dac8a75245a334530ea03b
nisunalade <aladenisun@yahoo.com>
2021-01-02 15:13:20 -0500

    change of posts pages

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import Home from '@/components/Pages/BlogArticle'
 import About from '@/views/About'
 import Application from '@/views/Application'
 import Contact from '@/views/Contact'
-import SignIn from '@/components/User/SignIn'
+// import SignIn from '@/components/User/SignIn'
 import BlogPost from '@/components/Pages/BlogPost'
 import ViewPosts from '@/components/Pages/AllPosts'
 import CreateBlogPost from '@/components/User/CreateBlogPost'
@@ -34,11 +34,11 @@ export default new VueRouter({
       name: 'Contact',
       component: Contact
     },
-    {
-      path: '/signIn',
-      name: 'SignIn',
-      component: SignIn
-    },
+    // {
+    //   path: '/signIn',
+    //   name: 'SignIn',
+    //   component: SignIn
+    //},
     {
       path: '/blogPost/new',
       name: 'CreateBlogPost',
